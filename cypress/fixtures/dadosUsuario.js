f42da5daa05bbc3e59ae85cab35ddf61cc8698d0
andreylobo <37642299+andreylobo@users.noreply.github.com>
2021-10-04 08:10:13 -0300

    criado arquivo para dados pessoais de um usuario

---
+++ cypress/fixtures/dadosUsuario.js
@@ -0,0 +1,13 @@
+export const DATA = {
+  name: "Teste Automação",
+  email: "andrey.loboo@gmail.com",
+  senha: "12345678",
+  rua: "Rua de Automação",
+  numero: "123",
+  bairro: "Teste",
+  cidade: "Curitiba",
+  cep: "80000000",
+  cpf: "57798320059",
+  dataNasc: "01012000",
+  fone: "41992720111"
+};
